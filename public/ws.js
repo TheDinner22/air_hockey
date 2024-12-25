2d140cc8f1bdf6f8680211ef415eb0571e53c092
Joseph Handsome <nottellingu@gmail.com>
2024-12-24 21:26:01 -0500

    added canvas and lsp is happy

--- public/ws.js
+++ public/ws.js
@@ -1,14 +1,23 @@
 var socket
+/**
+* @type {HTMLCanvasElement}
+*/
+var canvas
+/**
+* @type {CanvasRenderingContext2D}
+*/
+var ctx
 
 document.addEventListener("DOMContentLoaded", (_) => {
     console.log("hi")
-    ws()
+    setup_canvas()
+    // ws()
 });
 
 function ws(){
     socket = new WebSocket('ws://localhost:8000/ws');
 
-    socket.onopen = function(event) {
+    socket.onopen = function(_) {
         // Handle connection open
         console.log("open")
     };
@@ -18,11 +27,22 @@ function ws(){
         console.log(event.data)
     };
 
-    socket.onclose = function(event) {
+    socket.onclose = function(_) {
         // Handle connection close
         console.log("close")
     };
 
+}
+
+function setup_canvas (){
+    const c = document.getElementById("my-canvas")
+
+    // runtime code to make my lsp happy :(
+    if (c instanceof HTMLCanvasElement) {
+        canvas = c
+
+        ctx = canvas.getContext('2d')
+    }
 }
 
 function sendMessage(message) {
