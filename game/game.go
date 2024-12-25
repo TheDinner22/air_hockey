03c960f435612b7f339fb08b5d0b8b008fe89637
Joseph Handsome <nottellingu@gmail.com>
2024-12-25 18:41:22 -0500

    more types init functions and tests!

--- game/game.go
+++ game/game.go
@@ -1,7 +1,8 @@
 package game
 
 import (
-    "math"
+	"github.com/gorilla/websocket"
+	"math"
 )
 
 type Sizes struct {
@@ -9,25 +10,37 @@ type Sizes struct {
 	canvas_height int
 }
 
+type Point struct {
+	x int
+	y int
+}
+
+func NewPoint(x, y int) Point {
+	return Point{x, y}
+}
+
 type Circle struct {
-	x      int
-	y      int
+	center Point
 	radius int
 }
 
-func (c Circle) contains(other Circle) bool { // TODO test this!!!
-    diff_x := other.x - c.x
-    diff_y := other.y - c.y
-    center_dist := math.Sqrt(float64(diff_x*diff_x) + float64(diff_y*diff_y))
+func NewCircle(center Point, radius int) Circle {
+	return Circle{center, radius}
+}
 
-    longest_dist := c.radius + other.radius
+func (c Circle) contains(other Circle) bool {
+	diff_x := other.center.x - c.center.x
+	diff_y := other.center.y - c.center.y
+	center_dist := math.Sqrt(float64(diff_x*diff_x) + float64(diff_y*diff_y))
 
-    // if the centers are longest_dist aparart (or further), the circles don't overlap
-    if float64(longest_dist) > center_dist {
-        return true
-    } else {
-        return false
-    }
+	longest_dist := c.radius + other.radius
+
+	// if the centers are longest_dist aparart (or further), the circles don't overlap
+	if float64(longest_dist) > center_dist {
+		return true
+	} else {
+		return false
+	}
 }
 
 type Player struct {
@@ -36,14 +49,28 @@ type Player struct {
 	pos   Circle
 }
 
+func NewPlayer(name string, score int, pos Circle) Player {
+	return Player{name, score, pos}
+}
+
 type Puck struct {
 	pos      Circle
-	velocity Circle // in this case point is more like a vector dx, dy not x, y
+	velocity Point // in this case point is more like a vector dx, dy not x, y
+}
+
+func NewPuck(pos Circle, velocity Point) Puck {
+	return Puck{pos, velocity}
 }
 
 type GameState struct {
-	p1         Player
-	p2         Player
-	puck       Puck
-	game_sizes Sizes
+	P1         Player
+	P2         Player
+	Puck       Puck
+	Game_sizes Sizes
+	P1_conn    *websocket.Conn
+	P2_conn    *websocket.Conn
+}
+
+func NewGameState(p1 Player, p2 Player, puck Puck, game_sizes Sizes, p1_conn *websocket.Conn, p2_conn *websocket.Conn) GameState {
+	return GameState{p1, p2, puck, game_sizes, p1_conn, p2_conn}
 }
