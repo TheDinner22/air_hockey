aa840e63c9d02873cef3eeedcd56c7dcdbee5b26
Joseph Handsome <nottellingu@gmail.com>
2024-12-24 23:20:28 -0500

    ughhhhhh

--- public/ws.js
+++ public/ws.js
@@ -9,10 +9,36 @@ var canvas
 */
 var ctx
 
+class GameState {
+    constructor(){
+        this.player_a = -1
+        this.player_b = -1
+    }
+}
+
 document.addEventListener("DOMContentLoaded", (_) => {
     setup_canvas()
+
+    init_game()
+
     // ws()
 });
+
+function setup_canvas (){
+    const c = document.getElementById("my-canvas")
+
+    // runtime code to make my lsp happy :(
+    // TODO remove this in prod??? idk why not
+    if (c instanceof HTMLCanvasElement) {
+        canvas = c
+        ctx = canvas.getContext('2d')
+    }
+
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function init_game(){}
 
 function ws(){
     socket = new WebSocket('ws://localhost:8000/ws');
@@ -37,18 +63,3 @@ function ws(){
 function sendMessage(message) {
     socket.send(message);
 }
-
-function setup_canvas (){
-    const c = document.getElementById("my-canvas")
-
-    // runtime code to make my lsp happy :(
-    // TODO remove this in prod??? idk why not
-    if (c instanceof HTMLCanvasElement) {
-        canvas = c
-        ctx = canvas.getContext('2d')
-    }
-
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-}
-
